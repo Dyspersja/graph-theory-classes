2382dec9935a8966d89b61e7ad8d590134819c2b
Radosław Staniów <dyspersjamusic@gmail.com>
2025-03-24 18:18:04 +0100

    Matrix Class

---
+++ src/graph-theory-classes/src/main/java/org/example/matrix/Matrix.java
@@ -0,0 +1,34 @@
+package org.example.matrix;
+
+class Matrix {
+    private final int[][] data;
+
+    public Matrix(int rows, int cols) {
+        this.data = new int[rows][cols];
+    }
+
+    public void setValue(int row, int col, int value) {
+        data[row][col] = value;
+    }
+
+    public int getValue(int row, int col) {
+        return data[row][col];
+    }
+
+    public int[][] toArray() {
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int[] row : data) {
+            sb.append('|');
+            for (int value : row) {
+                sb.append(' ').append(value);
+            }
+            sb.append(" |\n");
+        }
+        return sb.toString();
+    }
+}
